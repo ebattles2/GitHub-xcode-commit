648416dafc881a9bfaab5b8402f25c9a5db452f1
Ericka :) <ebattles2@student.gsu.edu>
2017-06-05 12:27:04 -0400

    Initial Commit

---
+++ firstCalculatorApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firstCalculatorApp
+//
+//  Created by Ericka Battles on 6/5/17.
+//  Copyright © 2017 Ericka Battles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
